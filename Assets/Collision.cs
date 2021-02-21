86398de8f76d9f25362d36c7b68c31ad7b85ee3b
Alvin Wang <alvinwang922@gmail.com>
2021-02-20 16:24:21 -0800

    Implemented collision con color change

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -20,14 +20,20 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter(UnityEngine.Collision collision)
     {
         //Output the Collider's GameObject's name
-        Debug.Log(Collision.GetComponent<Collider>().name);
-        renderReference.color = Color.green;
+        UnityEngine.Debug.Log("YAY");
+        if (collision.gameObject.name == "Player") {
+            UnityEngine.Debug.Log("YAY");
+            GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 
     //If your GameObject keeps colliding with another GameObject with a Collider, do something
     void OnCollisionStay(UnityEngine.Collision collision)
     {
-        Debug.Log(Collision.GetComponent<Collider>().name);
-        renderReference.color = Color.green;
+        UnityEngine.Debug.Log("YAY");
+        if (collision.gameObject.name == "Player") {
+            UnityEngine.Debug.Log("LONGER");
+            GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityEngine.Debug.Log("TEST");
     }
 
     // Update is called once per frame
@@ -22,5 +23,22 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        //Output the Collider's GameObject's name
+        if (collision.gameObject.name == "Cube") {
+            UnityEngine.Debug.Log("YAY");
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    //If your GameObject keeps colliding with another GameObject with a Collider, do something
+    void OnCollisionStay(UnityEngine.Collision collision)
+    {
+        if (collision.gameObject.name == "Cube") {
+            UnityEngine.Debug.Log("LONGER");
+            GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 }

b5c404e755c8c3789cd3037bf9f316e66b7ae7f7
rasfmar <38634617+rasfmar@users.noreply.github.com>
2021-02-21 05:40:37 -0500

    shopping cart integration for meat (temp)

---
+++ Assets/Scripts/shoppingCartCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class shoppingCartCollision : MonoBehaviour
+{
+    SpriteRenderer renderReference;
+    private deteriorate deter;
+    private System.Random rand;
+
+    void Start()
+    {
+        deter = GameObject.Find("/Scripts").GetComponent<deteriorate>();
+        rand = new System.Random();
+    }
+
+    //If your GameObject keeps colliding with another GameObject with a Collider, do something
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+
+        if (collision.gameObject.name == "Meat") {
+            UnityEngine.Debug.Log("YAY");
+            GetComponent<Renderer>().material.color = Color.yellow;
+            foreach(GameObject obj in GameObject.FindObjectsOfType<GameObject>()) {
+                if (obj.name == "Tree") {
+                    foreach(Transform child in obj.GetComponentsInChildren<Transform>()) {
+                        deter.StartCoroutine(deter.Deter(
+                            child.gameObject,
+                            child.gameObject.name == "Trunk" ? 0.25f : (float)rand.NextDouble(),
+                            (float)rand.NextDouble() * 2f + 2f,
+                            child.gameObject.name == "Trunk" ? 0 : 1,
+                            1f - (float)rand.NextDouble() * .5f
+                        ));
+                    };
+                }
+            }
+        }
+    }
+}

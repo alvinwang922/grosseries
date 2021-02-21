55613229f32c935a9fbd9af589d1596364b1544f
Alvin Wang <alvinwang922@gmail.com>
2021-02-20 19:05:41 -0800

    Implemented object collision color change

---
+++ Assets/MeatCol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatCol : MonoBehaviour
+{
+    SpriteRenderer renderReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        //Output the Collider's GameObject's name
+        UnityEngine.Debug.Log("YAY");
+        if (collision.gameObject.name == "Player") {
+            UnityEngine.Debug.Log("YAY");
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    //If your GameObject keeps colliding with another GameObject with a Collider, do something
+    void OnCollisionStay(UnityEngine.Collision collision)
+    {
+        UnityEngine.Debug.Log("YAY");
+        if (collision.gameObject.name == "Player") {
+            UnityEngine.Debug.Log("LONGER");
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
